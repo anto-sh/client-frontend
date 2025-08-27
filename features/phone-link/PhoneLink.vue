d60c4a3c6eb6f3e3b50a905efe37bbb73ac180f7
antosh <tomatomoving@mail.ru>
2025-08-27 15:18:52 +0300

    feat: added phone link component

---
+++ features/phone-link/PhoneLink.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex items-center space-x-2">
+    <i class="pi pi-phone text-primary-400"></i>
+    <a href="tel:+79138886463" class="text-gray-300 hover:text-primary-400 transition-colors">
+      +7 (913) 888-64-63
+    </a>
+  </div>
+</template>
