e5d44f39a63ce3c679b15e508138ceb70c7260a2
antosh <tomatomoving@mail.ru>
2025-08-27 15:29:12 +0300

    feat: added treatments list component

--- features/treatments-list/TreatmentsList.vue
+++ features/treatments-list/TreatmentsList.vue
@@ -1,16 +1,26 @@
 <script setup lang="ts">
 import { useTreatmentsListModel } from './useTreatmentsListModel'
-const { data: treatmentsData, pending, error } = useTreatmentsListModel()
+const { data: treatmentsData } = useTreatmentsListModel()
 </script>
+
 <template>
-  <div v-if="pending">ЗАГРУЗКА</div>
-  <div v-else-if="error">ОШИБКА</div>
-  <div v-else>
-    <ul>
-      <li v-for="item in treatmentsData" :key="item.id">
-        {{ item.name }}
-      </li>
-      <Button label="Profile" icon="pi pi-user" severity="info" raised rounded size="large" class="p-4"></Button>
-    </ul>
-  </div>
+  <section>
+    <div class="mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
+        <Card
+          v-for="item in treatmentsData"
+          :key="item.id"
+          class="transition-shadow duration-300 hover:shadow-lg text-center bg-surface-900"
+        >
+          <template #title>
+            <div class="flex items-center space-x-3">
+              <span class="text-xl font-semibold">{{ item.name }}</span>
+            </div>
+          </template>
+        </Card>
+      </div>
+    </div>
+  </section>
 </template>
+
+<style></style>
