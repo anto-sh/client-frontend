9478d23145b75d24e5b45549999276c8f11d263e
antosh <tomatomoving@mail.ru>
2025-08-27 15:13:26 +0300

    feat: nuxt cache error handling

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// Обработка ошибки вследствие неверного кэширования чанков при некоторых переходах между страницами
+const router = useRouter()
+router.onError((error, to) => {
+  if (
+    error.message.includes('Failed to fetch dynamically imported module') ||
+    error.message.includes('error loading dynamically imported module')
+  ) {
+    // Принудительная перезагрузка страницы
+    window.location.href = to.fullPath
+  }
+})
+</script>
 
 <template>
   <div>
